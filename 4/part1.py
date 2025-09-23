18b6022858aa5116c8bb08ed5e68e5c366f13f3d
Lily Bevan <lilybevan31@gmail.com>
2025-09-23 12:28:13 +0100

    solve day 4

---
+++ 4/part1.py
@@ -0,0 +1,12 @@
+import hashlib
+
+inputStr = "bgvyzdsv"
+# inputStr = "abcdef"
+num = 0
+solved = False
+while not solved:
+    hashAttempt = hashlib.md5(f"{inputStr}{str(num)}".encode()).hexdigest()
+    num+=1
+    if hashAttempt[:5] == "00000":
+        print(num-1)
+        solved = True
