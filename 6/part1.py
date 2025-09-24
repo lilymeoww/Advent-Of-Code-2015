2cc23ac0254682e1873cacda6343bebb927bb069
Lily Bevan <lilybevan31@gmail.com>
2025-09-24 11:23:55 +0100

    framework for day 6

---
+++ 6/part1.py
@@ -0,0 +1,21 @@
+with open("input.txt", "r") as inputFile:
+    inputRaw = inputFile.readlines()
+    for lineIndex in range(len(inputRaw)):
+        inputRaw[lineIndex] = inputRaw[lineIndex].strip()
+
+lightsArray = [["0"]*1000]*1000
+
+def toggle(coord1X, coord1Y, coord2X, coord2Y, array):
+    pass
+
+def turnOn(coord1X, coord1Y, coord2X, coord2Y, array):
+    pass
+
+def turnOff(coord1X, coord1Y, coord2X, coord2Y, array):
+    pass
+
+
+# coord1X = coord1.split(",")[0]
+# coord1Y = coord1.split(",")[1]
+# coord2X = coord2.split(",")[0]
+# coord2Y = coord2.split(",")[1]
