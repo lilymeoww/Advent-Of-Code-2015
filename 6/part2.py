fa0957094b1f6b9e571098dbe8cda130c8356ace
Lily Bevan <lilybevan31@gmail.com>
2025-09-25 12:29:44 +0100

    solve day 6

---
+++ 6/part2.py
@@ -0,0 +1,47 @@
+with open("input.txt", "r") as inputFile:
+    inputRaw = inputFile.readlines()
+    for lineIndex in range(len(inputRaw)):
+        inputRaw[lineIndex] = inputRaw[lineIndex].strip()
+
+lightsArray = [[0]*1000 for _ in range(1000)]
+
+def toggle(coord1X, coord1Y, coord2X, coord2Y):
+    for deltaX in range((coord2X-coord1X)+1):
+        for deltaY in range((coord2Y-coord1Y)+1):
+            lightsArray[coord1Y + deltaY][coord1X + deltaX] += 2
+
+def turnOn(coord1X, coord1Y, coord2X, coord2Y):
+    for deltaX in range((coord2X-coord1X)+1):
+        for deltaY in range((coord2Y-coord1Y)+1):
+            lightsArray[coord1Y+deltaY][coord1X+deltaX] += 1
+
+
+def turnOff(coord1X, coord1Y, coord2X, coord2Y):
+    for deltaX in range((coord2X-coord1X)+1):
+        for deltaY in range((coord2Y-coord1Y)+1):
+            if lightsArray[coord1Y+deltaY][coord1X+deltaX] > 0:
+                lightsArray[coord1Y+deltaY][coord1X+deltaX] -= 1
+
+for instruction in inputRaw:
+    if "turn off" in instruction:
+        turnOff(int(instruction.split(" ")[2].split(",")[0]),
+                int(instruction.split(" ")[2].split(",")[1]),
+                int(instruction.split(" ")[4].split(",")[0]),
+                int(instruction.split(" ")[4].split(",")[1]))
+    elif "turn on" in instruction:
+        turnOn(int(instruction.split(" ")[2].split(",")[0]),
+                int(instruction.split(" ")[2].split(",")[1]),
+                int(instruction.split(" ")[4].split(",")[0]),
+                int(instruction.split(" ")[4].split(",")[1]))
+    elif "toggle" in instruction:
+        toggle(int(instruction.split(" ")[1].split(",")[0]),
+                int(instruction.split(" ")[1].split(",")[1]),
+                int(instruction.split(" ")[3].split(",")[0]),
+                int(instruction.split(" ")[3].split(",")[1]))
+
+brightnessSum = 0
+for row in lightsArray:
+    for light in row:
+        brightnessSum += light
+
+print(brightnessSum)
