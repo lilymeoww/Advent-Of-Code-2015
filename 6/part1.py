daa1cf1806382194273f39c360b4eb21fc1fab53
Lily Bevan <lilybevan31@gmail.com>
2025-09-25 12:26:15 +0100

    solve day 6 part 1 yeppers

--- 6/part1.py
+++ 6/part1.py
@@ -3,29 +3,51 @@ with open("input.txt", "r") as inputFile:
     for lineIndex in range(len(inputRaw)):
         inputRaw[lineIndex] = inputRaw[lineIndex].strip()
 
-lightsArray = [[0]*1000]*1000
+lightsArray = [[0]*1000 for _ in range(1000)]
 
-def toggle(coord1X, coord1Y, coord2X, coord2Y, array):
-    for xStep in range(coord2X-coord1X):
-        for yStep in range(coord2Y-coord1Y):
-            print(array[coord1Y + yStep][coord1X + xStep])
-            if array[coord1Y + yStep][coord1X + xStep] == 0:
-                array[coord1Y + yStep][coord1X + xStep] = 1
-                # print(f"{coord1X+xStep},{coord1Y+yStep} toggled from 0 to 1.")
-            elif array[coord1Y + yStep][coord1X + xStep] == 1:
-                array[coord1Y + yStep][coord1X + xStep] = 0
-                # print(f"{coord1X + xStep},{coord1Y + yStep} toggled from 1 to 0.")
+def toggle(coord1X, coord1Y, coord2X, coord2Y):
+    for deltaX in range((coord2X-coord1X)+1):
+        for deltaY in range((coord2Y-coord1Y)+1):
+            if lightsArray[coord1Y+deltaY][coord1X+deltaX] == 1:
+                lightsArray[coord1Y+deltaY][coord1X+deltaX] = 0
+                # print(f"({coord1X+deltaX},{coord1Y+deltaY}) toggled to 0.")
+            elif lightsArray[coord1Y+deltaY][coord1X+deltaX] == 0:
+                lightsArray[coord1Y+deltaY][coord1X+deltaX] = 1
+                # print(f"({coord1X + deltaX},{coord1Y + deltaY}) toggled to 1.")
 
-def turnOn(coord1X, coord1Y, coord2X, coord2Y, array):
-    pass
 
-def turnOff(coord1X, coord1Y, coord2X, coord2Y, array):
-    pass
+def turnOn(coord1X, coord1Y, coord2X, coord2Y):
+    for deltaX in range((coord2X-coord1X)+1):
+        for deltaY in range((coord2Y-coord1Y)+1):
+            lightsArray[coord1Y+deltaY][coord1X+deltaX] = 1
 
-toggle(0, 0, 100, 100, lightsArray)
-print(lightsArray[99][99])
-print(lightsArray[101][101])
-# coord1X = coord1.split(",")[0]
-# coord1Y = coord1.split(",")[1]
-# coord2X = coord2.split(",")[0]
-# coord2Y = coord2.split(",")[1]
+
+def turnOff(coord1X, coord1Y, coord2X, coord2Y):
+    for deltaX in range((coord2X-coord1X)+1):
+        for deltaY in range((coord2Y-coord1Y)+1):
+            lightsArray[coord1Y+deltaY][coord1X+deltaX] = 0
+
+for instruction in inputRaw:
+    if "turn off" in instruction:
+        turnOff(int(instruction.split(" ")[2].split(",")[0]),
+                int(instruction.split(" ")[2].split(",")[1]),
+                int(instruction.split(" ")[4].split(",")[0]),
+                int(instruction.split(" ")[4].split(",")[1]))
+    elif "turn on" in instruction:
+        turnOn(int(instruction.split(" ")[2].split(",")[0]),
+                int(instruction.split(" ")[2].split(",")[1]),
+                int(instruction.split(" ")[4].split(",")[0]),
+                int(instruction.split(" ")[4].split(",")[1]))
+    elif "toggle" in instruction:
+        toggle(int(instruction.split(" ")[1].split(",")[0]),
+                int(instruction.split(" ")[1].split(",")[1]),
+                int(instruction.split(" ")[3].split(",")[0]),
+                int(instruction.split(" ")[3].split(",")[1]))
+
+onCount = 0
+for row in lightsArray:
+    for light in row:
+        if light == 1:
+            onCount += 1
+
+print(onCount)
