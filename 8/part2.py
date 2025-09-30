93b5cf39a10382c2e062da708a5baf465bd33328
Lily Bevan <lilybevan31@gmail.com>
2025-09-30 10:08:26 +0100

    solve d8 p2

--- 8/part2.py
+++ 8/part2.py
@@ -6,38 +6,24 @@ with open("input.txt", "r") as inputFile:
 runningTotalString = 0
 runningTotalNew = 0
 for line in inputRaw:
-    charCount = 0
-    containsHex = True
-    containsBackslash = True
-    containsQuote = True
+    newString = ""
+    for char in line:
+        if char == '"':
+            newString += '\\"'
+        elif char ==  '\\':
+            newString += '\\\\'
+        else:
+            newString += char
 
+    newString = '"' + newString + '"'
+
+    runningTotalNew += len(newString)
     runningTotalString += len(line)
+    print(newString)
 
-    while containsQuote:
-        if '"' in line[1:-1]:
-            charCount += 1
-            temp = line[1:-1].replace('\\\"', "", 1)
-            line = '"' + temp + '"'
-        else:
-            containsQuote = False
-    while containsBackslash:
-        if '\\\\' in line[1:-1]:
-            charCount += 1
-            temp = line[1:-1].replace('\\\\', "", 1)
-            line = '"' + temp + '"'
-        else:
-            containsBackslash = False
-    while containsHex:
-        if "\\x" in line[1:-1]:
-            charCount += 1
-            temp = line[1:-1].replace(line[1:-1][line[1:-1].find('\\x'):line[1:-1].find('\\x')+4], "", 1)
-            line = '"' + temp + '"'
-        else:
-            containsHex = False
-
-    runningTotalMemory += len(line)-2
-    runningTotalMemory += charCount
+print(runningTotalNew-runningTotalString)
 
 
-print(runningTotalString-runningTotalMemory)
+
+
 
