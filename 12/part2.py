2076514c09373d08163cc4825607f95af08ff2e7
Lily Bevan <lilybevan31@gmail.com>
2025-10-01 11:53:16 +0100

    cook

---
+++ 12/part2.py
@@ -0,0 +1,24 @@
+import json
+
+def sum_all_integers(data):
+    total = 0
+    if isinstance(data, int):
+        total += data
+    elif isinstance(data, list):
+        for item in data:
+            total += sum_all_integers(item)
+    elif isinstance(data, dict):
+        if "red" in data.values():
+            return 0
+        for value in data.values():
+            total += sum_all_integers(value)
+    return total
+
+with open("input.json") as jsonDataRaw:
+    jsonData = json.loads(jsonDataRaw.readlines()[0].strip())
+
+result = sum_all_integers(jsonData)
+print(result)
+
+
+
