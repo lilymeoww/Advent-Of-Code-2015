2cae3017707f545aea59590ce57c41622ee938b9
Lily Bevan <lilybevan31@gmail.com>
2025-12-02 11:03:56 +0000

    Update part1.py

--- 15/part1.py
+++ 15/part1.py
@@ -2,3 +2,21 @@ with open("input.txt", "r") as inputFile:
     inputRaw = inputFile.readlines()
     for lineIndex in range(len(inputRaw)):
         inputRaw[lineIndex] = inputRaw[lineIndex].strip()
+
+ingredients = {}
+for line in inputRaw:
+    ingredients[line.split(":")[0]] = (int(line.split(" ")[2].strip(",")),
+                                       int(line.split(" ")[4].strip(",")),
+                                       int(line.split(" ")[6].strip(",")),
+                                       int(line.split(" ")[8].strip(",")),
+                                       int(line.split(" ")[10].strip(",")))
+
+
+combinations = []
+for i in range(101):
+    for j in range(101 - i):
+        for k in range(101 - i - j):
+            l = 100 - i - j - k
+            combinations.append([i, j, k, l])
+
+
