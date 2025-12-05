ce6f664ad4222901db6ae5d8a09de9735f810cd6
Lily Bevan <lilybevan31@gmail.com>
2025-12-05 00:10:54 +0000

    Create part2.py

---
+++ 17/part2.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+containers = [11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1, 19, 3]
+count = 0
+maxR = len(containers)
+
+for r in range(1, len(containers) + 1):
+    for combination in combinations(containers, r):
+        if sum(combination) == 150 and r < maxR:
+            maxR = r
+
+for combination in combinations(containers, maxR):
+    if sum(combination) == 150:
+        count += 1
+
+print(count)
