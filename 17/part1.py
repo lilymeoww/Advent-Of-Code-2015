82a92a775cd97c922b5351ef87aa76179e8e3079
Lily Bevan <lilybevan31@gmail.com>
2025-12-05 00:09:14 +0000

    Update part1.py

--- 17/part1.py
+++ 17/part1.py
@@ -1,4 +1,11 @@
-with open("input.txt", "r") as inputFile:
-    inputRaw = inputFile.readlines()
-    for lineIndex in range(len(inputRaw)):
-        inputRaw[lineIndex] = inputRaw[lineIndex].strip()
+from itertools import combinations
+
+containers = [11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1, 19, 3]
+count = 0
+
+for r in range(1, len(containers) + 1):
+    for combination in combinations(containers, r):
+        if sum(combination) == 150:
+            count += 1
+
+print(count)
